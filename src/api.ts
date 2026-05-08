db179e4e251ceae20fefaef4f7584b2939cbb68a
cutsweettea <swagastan@proton.me>
2026-05-08 19:41:59 -0400

    cryptojs aes256 decrypt pages

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,11 @@ const app = express();
 const PORT = 4567;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.sendFile('/home/amen/panel/src/site/pages/main.html');
+    res.sendFile('/home/amen/panel/src/site/index.html');
+});
+
+app.get('/main', (req: express.Request, res: express.Response) => {
+    res.sendFile('/home/amen/panel/src/site/pages/main');
 });
 
 app.listen(PORT, () => {
