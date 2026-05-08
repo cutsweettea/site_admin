1f9a96a5251537638f0b5db85962fc51affe27e3
cutsweettea <swagastan@proton.me>
2026-05-08 19:12:21 -0400

    made sendfile path absolute

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 4567;
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.sendFile('./site/pages/main.html');
+    res.sendFile('/home/amen/panel/src/site/pages/main.html');
 });
 
 app.listen(PORT, () => {
